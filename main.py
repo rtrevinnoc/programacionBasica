d9e34d7afcfefeb42ecc93f744aa9905cb4fb8f1
Roberto Treviño Cervantes <rtrevinnoc@icloud.com>
2026-08-07 20:00:09 -0600

    Tercera clase

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+opciones = [
+    "Salir",
+    "Detalles",
+    "Sumar"
+]
+
+def ejecutar_detalles():
+    print("--### DETALLES ###---")
+    print("Programa de ejemplo del salón 403")
+    print("Escrito por Roberto Treviño Cervantes")
+
+def ejecutar_salida():
+    return
+
+def ejecutar_suma():
+    return 2 + 2
+
 def seleccionar_opcion(opcion):
-    print(opcion)
+    if opcion == 0:
+        ejecutar_salida()
+    elif opcion == 2:
+        print(ejecutar_suma())
+    else:
+        ejecutar_detalles()
 
 def main():
     print("Iniciando Sistema...")
     print("--### MENU ###--")
-    print("0) Salir")
-    print("1) Detalles")
+    print("0) " + opciones[0])
+    print("1) " + opciones[1])
+    print("2) " + opciones[2])
 
-    entrada = input("Selecciona una opción: ")
+    entrada = int(input("Selecciona una opción: "))
     seleccionar_opcion(entrada)
 
 main()
