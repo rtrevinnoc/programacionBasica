8c3f8eb910b8500e11676622d596fac990193d39
Roberto Treviño Cervantes <rtrevinnoc@hotmail.com>
2026-08-11 19:51:48 -0600

    Tercera clase

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
-def seleccionar_opcion(opcion):
-    print(opcion)
+def ejecutar_detalles():
+    print("--### DETALLES ###--")
+    print("Programa de ejemplos del salón 403")
+    print("Escrito por Roberto Treviño Cervantes")
+ 
+
+def ejecutar_salida():
+    return
+ 
+
+def ejecutar_suma():
+    print(2 + 2)
+
+
+def ejecutar_resta():
+    print(4 - 2)
+
+
+opciones = {
+    "Salir": ejecutar_salida,
+    "Detalles": ejecutar_detalles,
+    "Sumar": ejecutar_suma,
+    "Restar": ejecutar_resta,
+}
+opciones_lista = list(opciones)
+numero_opciones = len(opciones)
+
+
+def ejecutar_seleccion(opcion):
+    seleccion = opciones.get(opcion, None)
+    
+    if seleccion == None:
+        seleccion_numero = int(opcion)
+
+        if seleccion_numero < numero_opciones:
+            seleccion = opciones_lista[seleccion_numero]
+
+            funcion_seleccionada = opciones[seleccion]
+            funcion_seleccionada()
+        else:
+            print("#=> No existe la opción")
+
+    else:
+        funcion_seleccionada = seleccion
+        funcion_seleccionada()
+
 
 def main():
     print("Iniciando Sistema...")
     print("--### MENU ###--")
-    print("0) Salir")
-    print("1) Detalles")
 
-    entrada = input("Selecciona una opción: ")
-    seleccionar_opcion(entrada)
+    for indice, opcion in enumerate(opciones):
+        print(f"{indice}) {opcion}")
+ 
+    opcion = input("Selecciona una opcion: ")
+    ejecutar_seleccion(opcion)
 
+ 
 main()
