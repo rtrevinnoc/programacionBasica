302d8f1d319b0ebd9c6d306e0a26b894fa116c93
Roberto Treviño Cervantes <rtrevinnoc@icloud.com>
2026-09-08 19:28:58 -0600

    reescritura

---
+++ division.py
@@ -0,0 +1,5 @@
+def division(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Aqui se define la division")
--- main.py
+++ main.py
@@ -1,69 +1,24 @@
-class Calculadora:
-    def __init__(self):
-        pass
+from division import division
+from multiplicacion import multiplicacion
+from resta import resta
+from suma import suma
+from entrada import recibir_opcion, recibir_entrada
 
-    def sumar(self):
-        pass
+if __name__ == "__main__":
+    print(f"{"*" * 6} MENU {"*" * 6}")
 
-    def restar(self):
-        pass
+    opciones = {
+        "suma": suma,
+        "resta": resta,
+        "divison": division,
+        "multiplicacion": multiplicacion
+    }
 
+    opcion = recibir_opcion("Introduce una opción: ", opciones)
 
-def ejecutar_detalles():
-    print("--### DETALLES ###--")
-    print("Programa de ejemplos del salón 403")
-    print("Escrito por Roberto Treviño Cervantes")
- 
+    a = recibir_entrada("Introduce un numero: ")
+    b = recibir_entrada("Introduce otro numero: ")
 
-def ejecutar_salida():
-    return
+    resultado = opciones[opcion](a, b)
 
-
-def input_entero(prompt):
-    return int(input(prompt))
-
-
-def operacion_aritmetica():
-    pass
-
-opciones = {
-    "Salir": ejecutar_salida,
-    "Detalles": ejecutar_detalles,
-    "Sumar": operacion_aritmetica,
-    "Restar": operacion_aritmetica,
-}
-opciones_lista = list(opciones)
-numero_opciones = len(opciones)
-
-
-def ejecutar_seleccion(opcion):
-    seleccion = opciones.get(opcion, None)
-    
-    if seleccion == None:
-        seleccion_numero = int(opcion)
-
-        if seleccion_numero < numero_opciones:
-            seleccion = opciones_lista[seleccion_numero]
-
-            funcion_seleccionada = opciones[seleccion]
-            funcion_seleccionada()
-        else:
-            print("#=> No existe la opción")
-
-    else:
-        funcion_seleccionada = seleccion
-        funcion_seleccionada()
-
-
-def main():
-    print("Iniciando Sistema...")
-    print("--### MENU ###--")
-
-    for indice, opcion in enumerate(opciones):
-        print(f"{indice}) {opcion}")
- 
-    opcion = input("Selecciona una opcion: ")
-    ejecutar_seleccion(opcion)
-
- 
-main()
+    print(f"#=> {resultado}")
